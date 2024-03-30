8479b9d10014ef89aa3791c3c7cb1fa7e660f724
Tom <JosephDotson@belkonar.com>
2024-03-30 16:59:46 -0400

    updates

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {Button} from "@mui/material";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
         <div>hi</div>
+        <Button variant="contained">Contained</Button>
     </React.StrictMode>
 );
